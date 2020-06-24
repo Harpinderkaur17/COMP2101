c155e7016ad8f7df22507449047820863cbca73b
Harpinder kaur <harpinderkaur17@yahoo.com>
2020-06-24 15:28:28 -0700

    lab 3

---
+++ bash/login-welcome.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# This script produces a dynamic welcome message
+# it should look like
+#   Welcome to planet hostname, title name!
+
+# Task 1: Use the variable $USER instead of the myname variable to get your name
+# Task 2: Dynamically generate the value for your hostname variable using the hostname command - e.g. $(hostname)
+# Task 3: Add the time and day of the week to the welcome message using the format shown below
+#   Use a format like this:
+#   It is weekday at HH:MM AM.
+# Task 4: Set the title using the day of the week
+#   e.g. On Monday it might be Optimist, Tuesday might be Realist, Wednesday might be Pessimist, etc.
+#   You will need multiple tests to set a title
+#   Invent your own titles, do not use the ones from this example
+
+###############
+# Variables   #
+###############
+title="Overlord"
+#Task1 : used variable $USER
+myname=$USER
+#Task2: using hostname command got hostname
+hostname=$(hostname)
+#Task3: Added time and day of week
+today=$(date +%A)
+time=$(date '+%I:%M %p')
+
+#Task 4: setting title using day of week
+doa=$(date +%w)
+
+if [ $doa -eq 0 ];
+then
+	title="Great"
+fi
+if [ $doa -eq 1 ];
+then
+	title="Overlord"
+fi
+if [ $doa -eq 2 ];
+then
+	title="Ranger"
+fi
+if [ $doa -eq 3 ];
+then
+	title="Master"
+fi
+if [ $doa -eq 4 ];
+then
+	title="Queen"
+fi
+if [ $doa -eq 5 ];
+then
+	title="Majesty"
+fi
+if [ $doa -eq 6 ];
+then
+	title="Handsome"
+fi
+###############
+# Main        #
+###############
+msgVar="Welcome to planet $hostname, $title $myname!, It is $today at $time"
+cowsay $msgVar
---
+++ bash/passwordguesser.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# This script demonstrates testing to see if 2 strings are the same
+# it also demonstrates using conditional execution
+
+# TASK 1: Improve it by asking the user for a password guess instead of using inline literal data (i.e. TestString)
+# TASK 2: Improve it by rewriting it to use the if command
+#         The if command should test if they got it right, in which case it should tell them they got it right
+#         The if command should tell them they got it wrong
+# TASK 3: Improve it by giving them 5 tries to get it right before failing
+#           *** Do not use the exit command, and do not use a loop (e.g. while or for)
+
+#myString="TestString"
+#task 1 user input fr password
+read -p "Enter Password (1/5):" myString 
+referenceString="password"
+# Task 2 Changed to if else condition
+#Task 3: used if else combinations to give user 5 tries
+if [ $myString = $referenceString ];
+then
+	echo "You got it right!" 
+else
+	echo "You got it wrong!"
+        read -p "Enter Password (2/5):" myString	
+	if [ $myString = $referenceString ];
+then
+        echo "You got it right!"
+else
+        echo "You got it wrong!"
+        read -p "Enter Password (3/5):" myString
+if [ $myString = $referenceString ];
+then
+        echo "You got it right!"
+else
+        echo "You got it wrong!"
+        read -p "Enter Password (4/5):" myString
+if [ $myString = $referenceString ];
+then
+        echo "You got it right!"
+else
+        echo "You got it wrong!"
+        read -p "Enter Password (5/5):" myString
+if [ $myString = $referenceString ];
+then
+        echo "You got it right!"
+else
+        echo "You got it wrong!, No More Tries"
+fi
+fi
+fi
+	fi
+fi
